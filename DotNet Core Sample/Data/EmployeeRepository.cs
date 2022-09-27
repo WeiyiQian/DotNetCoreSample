37107605f9ca8c71e1d5d3364c5925094f9aafae
weiyi qian <412117899@qq.com>
2022-09-27 16:58:48 -0500

    text

--- DotNet Core Sample/Data/EmployeeRepository.cs
+++ DotNet Core Sample/Data/EmployeeRepository.cs
@@ -6,12 +6,12 @@ namespace DotNet_Core_Sample.Data
     public class EmployeeRepository: IEmployeeRepository
     {
         List<Employee> DataSource= new List<Employee>()
-            {
+        {
                 new Employee() { ID = 1, Name = "April"},
                 new Employee() { ID = 2, Name = "Bob"}
-    };
+        };
 
-    public List<Employee> GetAll()
+        public List<Employee> GetAll()
         {
             return DataSource;
         }
